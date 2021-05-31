3b4279a91f6b7fde536d9d3cb3d42a9349478dbc
Nagaraj <nravinuthala@gmail.com>
2021-05-31 18:24:14 +0530

    refactoring

--- scripts/pytest1.py
+++ scripts/pytest1.py
@@ -2,15 +2,27 @@ import os
 from pybfe.client.session import Session
 from intentionet.bfe.proto import api_gateway_pb2 as api
 
-os.environ['BFE_SSL_CERT'] = "nexbfe.crt"
+os.environ['BFE_SSL_CERT'] = "../cert/nexbfe.crt"
 BFE_HOST = "batfish.nexariacloud.com"
 BFE_PORT = 443
 bf = Session(host=BFE_HOST, port=BFE_PORT)
+if bf:
+    print()
+    print("***********************************************")
+    print("Created BFE session on host {}".format(BFE_HOST))
+    print("***********************************************")
+    print()
 
 REF_SNAPSHOT = ''
 NEW_SNAPSHOT = ''
 NETWORK_NAME="bgp_test_lab"
 bf.set_network(NETWORK_NAME)
+print()
+print("***********************************************")
+print("Network is set as {}".format(NETWORK_NAME))
+print("***********************************************")
+print()
+
 
 snapshots = bf.list_snapshots()
 #case when tere are just 2 snapshots, where reference snapshot is called baseline and is manually uploaded
@@ -25,6 +37,12 @@ else:
             REF_SNAPSHOT = snapshot
             break
     NEW_SNAPSHOT = snapshots[0]
+
+print()
+print("***********************************************")
+print("Snapshots being compared are {}, {}".format(REF_SNAPSHOT, NEW_SNAPSHOT))
+print("***********************************************")
+print()
 
 def get_compare_metadata_results(bf: Session, snapshot_name: str, reference_snapshot_name: str):
     """
